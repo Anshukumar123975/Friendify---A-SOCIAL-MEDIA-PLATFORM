1f2699d39b3fc99cdad694a9ef6dc4803bbd636b
Anshukumar123975 <anshupat2020@gmail.com>
2025-01-16 20:56:33 +0530

    db connection

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectToDb = async() => {
     try{
-        const connectionInstance = await mongoose.connect("mongodb://localhost:27017");
+        const connectionInstance = await mongoose.connect("mongodb+srv://anshupat2020:cjta83Z9YNbRhG_@cluster0.4xaw1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
         console.log(`MongoDb connected: ${connectionInstance.connection.host}`)
     }
     catch(error){
